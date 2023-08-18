93c8a7bfbef0d28ea14a82317b1fed65f1273369
Jay H <mobeets@gmail.com>
2023-08-18 08:53:35 -0400

    papers

--- main_catch.py
+++ main_catch.py
@@ -37,7 +37,7 @@ plt.hist(ends, np.arange(0, env.screen_height,20), alpha=0.4)
 #%% initialize model
 
 use_custom_model = False
-env_config = {'gravity': 0, 'tau': 0.015, 'action_penalty': 0.0005, 'delay': 5}
+env_config = {'gravity': 0, 'tau': 0.015, 'action_penalty': 0.0005, 'delay': 0}
 
 env_name = 'catch'
 register_env(env_name, lambda env_config: DelayedCatchEnv(**env_config))
@@ -103,12 +103,15 @@ plt.plot([get_score(x) for x in outputs])
 
 #%% rollout
 
-algo.restore(checkpoints[-2])
+# algo.restore(checkpoints[-2])
+algo.restore(checkpoint_delay5)
 
 explore = False
 nepisodes = 250
+randomPolicy = True
 
 env = DelayedCatchEnv(**env_config)
+env.delay = 5
 
 Trials = []
 for j in range(nepisodes):
@@ -124,6 +127,8 @@ for j in range(nepisodes):
         action, state, q_info = algo.compute_single_action(obs,
                 prev_action=action if config.model['lstm_use_prev_action'] else None,
                 state=state, explore=explore)
+        if randomPolicy:
+            action = env.action_space.sample()
         new_obs, reward, terminated, truncated, info = env.step(action)
         trials.append((obs, state, action, reward, info))
         obs = new_obs
@@ -138,7 +143,7 @@ print(np.mean([trials[-1][-2] for trials in Trials]))
 for i, trials in enumerate(Trials):
     ball = np.vstack([trial[-1]['ball'][1] for trial in trials])
     hand = np.vstack([trial[-1]['hand'][1] for trial in trials])
-    if i == 0:
+    if i == 9:
         plt.plot(ball)
         plt.plot(hand)
         [plt.plot(x*np.ones(2), y + np.array([-1,1])*env.hand_length/2, 'k-', alpha=0.1, linewidth=1, zorder=-1) for x,y in enumerate(hand)]
@@ -149,3 +154,73 @@ plt.ylim([0, env.screen_height])
 # - use evaluation made of fixed trajectories?
 # - what are the signatures of prediction?
 # 
+
+#%% decoding future ball states from lstm
+
+from analysis.correlations import linreg_fit, linreg_eval
+from sklearn.decomposition import PCA
+
+delays = list(np.arange(-5, 10))
+Pts = {delay: ([], [], []) for delay in delays}
+
+Z = np.vstack([trial[1][0] for trials in Trials[:int(len(Trials)/2)] for trial in trials])
+pca = PCA(n_components=Z.shape[1])
+pca.fit(Z)
+# plt.plot(pca.explained_variance_ratio_[:10], '.-'), plt.ylim([0,1])
+
+for i, trials in enumerate(Trials):
+    isTrainTrial = True if i < len(Trials)/2 else False
+    # ball = np.vstack([trial[-1]['ball'] for trial in trials])
+    ball = np.vstack([trial[0][:2] for trial in trials])
+
+    hand = np.vstack([trial[-1]['hand'] for trial in trials])[:,1:]
+    lstm = np.vstack([trial[1][1] for trial in trials])
+
+    X = lstm; Y = ball
+    
+    for delay in delays:
+        if delay == 0:
+            Xc = X
+            Yc = Y
+        elif delay > 0:
+            Xc = X[:-delay]
+            Yc = Y[delay:]
+        elif delay < 0:
+            Xc = X[-delay:]
+            Yc = Y[:delay]
+        ixTrainc = np.array([isTrainTrial]*len(Yc))
+        Pts[delay][0].append(Xc)
+        Pts[delay][1].append(Yc)
+        Pts[delay][2].append(ixTrainc)
+for delay, (X, Y, I) in Pts.items():
+    Pts[delay] = (np.vstack(X), np.vstack(Y), np.hstack(I).astype(bool))
+
+def linreg(X, Y, ix):
+    mdl = linreg_fit(X[ix], Y[ix], scale=True, add_bias=True)
+    res = linreg_eval(X[~ix], Y[~ix], mdl)
+    return mdl, res
+
+pts = []
+for delay, (X, Y, ixTrain) in Pts.items():
+    # use LSTM's activity at time (t-delay) to predict state at time t
+    X = pca.transform(X)[:,:5]
+    mdl, res = linreg(X, Y, ixTrain)
+    pts.append((delay, res['rsq']))
+pts = np.vstack(pts)
+
+plt.plot(pts[:,0], pts[:,1], 'ko')
+plt.xlabel('delay')
+plt.ylabel('$R^2$')
+plt.ylim([-0.03,1.03])
+
+# %%
+
+# plt.plot(pts_delay0[:,0], pts_delay0[:,1], 'o', label='τ=0')
+# plt.plot(pts_delay5[:,0], pts_delay5[:,1], 'o', label='τ=5')
+# plt.plot(pts[:,0], pts[:,1], 'o', label='τ=5')
+plt.plot(pts_delay0_rand[:,0], pts_delay0_rand[:,1], 'o', label='τ=0')
+plt.plot(pts_delay5_rand[:,0], pts_delay5_rand[:,1], 'o', label='τ=5')
+plt.xlabel('delay (d)')
+plt.ylabel('$R^2$ using $Z_t$ to predict $O_{t+d}$')
+plt.ylim([-0.03,1.03])
+plt.legend(fontsize=8)
